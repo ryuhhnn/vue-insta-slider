dcfd8cf8cb28f21291fed569385fc49e8c5fc4b4
Ryan Coleman <me@ryuhhnn.com>
2018-03-28 18:25:31 -0500

    :lipstick: Fixed a styling issue where images were video thumbnails were showing up weird

--- src/components/SlideImage.vue
+++ src/components/SlideImage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="img-thumbnail">
-        <img :src="image.thumbnail_src" :alt="image.edge_media_to_caption.edges[0].node.text" />
+        <img width="640px" :src="image.thumbnail_src" :alt="image.edge_media_to_caption.edges[0].node.text" />
     </div>
 </template>
 
